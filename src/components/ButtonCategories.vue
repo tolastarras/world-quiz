7fc589dfee618ff57a4b23547f24b792b0ce2ce9
Rafael <tolastarras@gmail.com>
2018-12-16 23:21:08 -0500

    features/change-title-from-button-input: update title

---
+++ src/components/ButtonCategories.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="btn-group">
+    <button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <span class="fa fa-gamepad"></span> Category: Find {{ category }} <span class="caret"></span>
+    </button>
+    <ul class="dropdown-menu">
+      <li @click="selectCategory" class="list-item" :key="category" v-for="category in categories">{{ category }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  methods: {
+    selectCategory (e) {
+      this.$store.dispatch('changeCategory', e.target.innerText)
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'category',
+      'categories'
+    ])
+    // category () {
+    //   console.log(this.$store.getters.getCategory)
+    //   return this.$store.getters.getCategory
+    // },
+    // categories () {
+    //   return this.$store.getters.getCategories
+    // }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dropdown-menu {
+  background-color: #43ac6a;
+  border-color: #3c9a5f;
+}
+</style>
--- src/components/ButtonContinents.vue
+++ src/components/ButtonContinents.vue
@@ -3,7 +3,6 @@
     <button type="button" class="btn btn-primary dropdown-toggle mr-2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
       <span class="fa fa-globe"></span> Region: {{ continent }} <span class="caret"></span>
     </button>
-
     <ul class="dropdown-menu">
       <li @click="selectContinent" class="list-item" :key="continent" v-for="continent in continents">{{ continent }}</li>
     </ul>
@@ -12,30 +11,17 @@
 
 <script>
 export default {
-  data () {
-    return {
-      continents: [
-        'World',
-        'Africa',
-        'Asia',
-        'Europe',
-        'North America',
-        'South America',
-        'Oceania'
-      ]
-    }
-  },
-  mounted () {
-    // this.continent = this.continents[0]
-  },
   methods: {
     selectContinent (e) {
-      this.$store.dispatch('changeContinent', { continent: e.target.innerText })
+      this.$store.dispatch('changeContinent', e.target.innerText)
     }
   },
   computed: {
     continent () {
-      return this.$store.getters.getContinent.continent
+      return this.$store.getters.continent
+    },
+    continents () {
+      return this.$store.getters.continents
     }
   }
 }
@@ -45,22 +31,5 @@ export default {
 .dropdown-menu {
   background-color: #008cba;
   border-color: #0079a1;
-
-  li {
-    color: #ccc;
-    margin: 0 8px;
-    padding: 5px 0;
-    border-bottom: 1px solid #ccc;
-
-    &:hover {
-      color: #eee;
-      cursor: pointer;
-    }
-
-    &:last-child {
-      padding-bottom: 0;
-      border-bottom: 0;
-    }
-  }
 }
 </style>
