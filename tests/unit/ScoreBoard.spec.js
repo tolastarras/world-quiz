cf763395f63b6cf28818a854dcd5ec002d75223e
Rafael <tolastarras@gmail.com>
2018-12-29 23:59:21 -0500

    set up app for jest testing

--- tests/unit/ScoreBoard.spec.js
+++ tests/unit/ScoreBoard.spec.js
@@ -1,5 +1,5 @@
-import {mount} from '@vue/test-utils'
-import ScoreBoard from '@/components/ScoreBoard.vue'
+import { mount } from '@vue/test-utils'
+import ScoreBoard from '@/components/ScoreBoard'
 
 describe('ScoreBoard.vue', () => {
   it('setup correctly', () => {
